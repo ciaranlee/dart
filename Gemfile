1ea50066e3c551dfad4210a4dc97a2785c80c0aa
Ciaran Lee <ciaran.lee@gmail.com>
2010-12-12 23:56:32 +0000

    whitespace

--- Gemfile
+++ Gemfile
@@ -8,10 +8,10 @@ gem "json"
 gem "newrelic_rpm"
 
 group :test do
-	gem "rack-test"
-	gem "rspec"
-	gem "fakeweb"
-	gem 'autotest'
+  gem "rack-test"
+  gem "rspec"
+  gem "fakeweb"
+  gem 'autotest'
   gem 'autotest-fsevent'
   gem 'autotest-growl'
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,28 @@
 $('#stations a').click(function() {
-	var jq_obj = $(this);
-	options = {url:this.href};
+  var jq_obj = $(this);
+  options = {url:this.href};
   fetch_trains(options);
   return false;
 });
 
 function fetch_trains (options) {
-	var results_list = $('ul#results');
-	results_list.html('');
+  var results_list = $('#results');
+  results_list.html('');
   $.getJSON(options.url,
     function(json) {
       $(json.results).each(function(index) {
-				var li = $('<li></li>')
-					.append($('<dl></dl>')
-						.append('<dt>direction</dt><dd>'+this.direction+'</dd>')
-					  .append('<dt>service</dt><dd>'+this.service+'</dd>')
-					  .append('<dt>eta</dt><dd>'+this.eta+'</dd>')
-					  .append('<dt>scheduled</dt><dd>'+this.scheduled+'</dd>')
-					  .append('<dt>due</dt><dd>'+this.due+'</dd>')
-					  .append('<dt>service</dt><dd>'+this.service+'</dd>')
-					  .append('<dt>route</dt><dd>'+this.route+'</dd>')
-					  .append('<dt>info</dt><dd>'+this.info+'</dd>')
-					);
-				results_list.append(li);
+        var li = $('<li></li>')
+          .append($('<dl></dl>')
+            .append('<dt>direction</dt><dd>'+this.direction+'</dd>')
+            .append('<dt>service</dt><dd>'+this.service+'</dd>')
+            .append('<dt>eta</dt><dd>'+this.eta+'</dd>')
+            .append('<dt>scheduled</dt><dd>'+this.scheduled+'</dd>')
+            .append('<dt>due</dt><dd>'+this.due+'</dd>')
+            .append('<dt>service</dt><dd>'+this.service+'</dd>')
+            .append('<dt>route</dt><dd>'+this.route+'</dd>')
+            .append('<dt>info</dt><dd>'+this.info+'</dd>')
+          );
+        results_list.append(li);
       });
     }
   );
