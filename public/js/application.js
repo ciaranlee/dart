5c55a20cc5ff31827e0737041bdde32beed30e2f
Ciaran Lee <ciaran.lee@gmail.com>
2011-01-12 21:22:23 +0000

    js works with better results structure

--- public/js/application.js
+++ public/js/application.js
@@ -6,23 +6,27 @@ $('#stations a').click(function() {
 });
 
 function fetch_trains (options) {
-  var results_list = $('#results');
-  results_list.html('');
+  var results_div = $('#results');
+  results_div.html('');
   $.getJSON(options.url,
     function(json) {
       $(json.results).each(function(index) {
-        var li = $('<li></li>')
-          .append($('<dl></dl>')
-            .append('<dt>direction</dt><dd>'+this.direction+'</dd>')
-            .append('<dt>service</dt><dd>'+this.service+'</dd>')
-            .append('<dt>eta</dt><dd>'+this.eta+'</dd>')
-            .append('<dt>scheduled</dt><dd>'+this.scheduled+'</dd>')
-            .append('<dt>due</dt><dd>'+this.due+'</dd>')
-            .append('<dt>service</dt><dd>'+this.service+'</dd>')
-            .append('<dt>route</dt><dd>'+this.route+'</dd>')
-            .append('<dt>info</dt><dd>'+this.info+'</dd>')
-          );
-        results_list.append(li);
+        var direction_div = $('<div class="direction"><h2>'+this.direction+'</h2></div>');
+        var ol = $('<ol></ol>');
+        for (var i=0; i < this.trains.length; i++) {
+          var li = $('<li></li>')
+            .append($('<dl></dl>')
+              .append('<dt>service</dt><dd>'+this.trains[i].service+'</dd>')
+              .append('<dt>eta</dt><dd>'+this.trains[i].eta+'</dd>')
+              .append('<dt>scheduled</dt><dd>'+this.trains[i].scheduled+'</dd>')
+              .append('<dt>due</dt><dd>'+this.trains[i].due+'</dd>')
+              .append('<dt>route</dt><dd>'+this.trains[i].route+'</dd>')
+              .append('<dt>info</dt><dd>'+this.trains[i].info+'</dd>')
+            );
+            ol.append(li);
+        };
+        direction_div.append(ol);
+        results_div.append(direction_div);
       });
     }
   );
