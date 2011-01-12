c55ba0a3ceee9f60cd88a99e3f31a1437c78dcc7
Ciaran Lee <ciaran.lee@gmail.com>
2011-01-12 22:59:08 +0000

    error message if no trains

--- public/js/application.js
+++ public/js/application.js
@@ -11,22 +11,26 @@ function fetch_trains (options) {
   $.getJSON(options.url,
     function(json) {
       results_div.html('');
-      $(json.results).each(function(index) {
-        var direction_div = $('<div class="direction"><h2>'+this.direction+'</h2></div>');
-        var ol = $('<ol></ol>');
-        for (var i=0; i < this.trains.length; i++) {
-          var li = $('<li></li>')
-              .append('<dl><dt class="onecol">service</dt><dd>'+this.trains[i].service+'</dd><dl>')
-              .append('<dl><dt class="onecol">eta</dt><dd>'+this.trains[i].eta+'</dd><dl>')
-              .append('<dl><dt class="onecol">scheduled</dt><dd>'+this.trains[i].scheduled+'</dd><dl>')
-              .append('<dl><dt class="onecol">due</dt><dd>'+this.trains[i].due+'</dd><dl>')
-              .append('<dl><dt class="onecol">route</dt><dd>'+this.trains[i].route+'</dd><dl>')
-              .append('<dl><dt class="onecol">info</dt><dd>'+this.trains[i].info+'</dd><dl>');
-            ol.append(li);
-        };
-        direction_div.append(ol);
-        results_div.append(direction_div);
-      });
+      if (typeof(json.results) != "undefined") {
+        $(json.results).each(function(index) {
+          var direction_div = $('<div class="direction"><h2>'+this.direction+'</h2></div>');
+          var ol = $('<ol></ol>');
+          for (var i=0; i < this.trains.length; i++) {
+            var li = $('<li></li>')
+                .append('<dl><dt class="onecol">service</dt><dd>'+this.trains[i].service+'</dd><dl>')
+                .append('<dl><dt class="onecol">eta</dt><dd>'+this.trains[i].eta+'</dd><dl>')
+                .append('<dl><dt class="onecol">scheduled</dt><dd>'+this.trains[i].scheduled+'</dd><dl>')
+                .append('<dl><dt class="onecol">due</dt><dd>'+this.trains[i].due+'</dd><dl>')
+                .append('<dl><dt class="onecol">route</dt><dd>'+this.trains[i].route+'</dd><dl>')
+                .append('<dl><dt class="onecol">info</dt><dd>'+this.trains[i].info+'</dd><dl>');
+              ol.append(li);
+          };
+          direction_div.append(ol);
+          results_div.append(direction_div);
+        });
+      } else{
+        results_div.html('<h2>No trains</h2>');
+      };
     }
   );
 }
