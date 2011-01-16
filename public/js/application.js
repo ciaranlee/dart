8f7df9dd5719d1a0773c3a08e91dbb20e024b58f
Ciaran Lee <ciaran.lee@gmail.com>
2011-01-16 20:52:57 +0000

    fixing some html inserted by js

--- public/js/application.js
+++ public/js/application.js
@@ -18,12 +18,12 @@ function fetch_trains (options) {
           var ol = $('<ol></ol>');
           for (var i=0; i < this.trains.length; i++) {
             var li = $('<li></li>')
-                .append('<dl><dt class="onecol">service</dt><dd>'+this.trains[i].service+'</dd><dl>')
-                .append('<dl><dt class="onecol">eta</dt><dd>'+this.trains[i].eta+'</dd><dl>')
-                .append('<dl><dt class="onecol">scheduled</dt><dd>'+this.trains[i].scheduled+'</dd><dl>')
-                .append('<dl><dt class="onecol">due</dt><dd>'+this.trains[i].due+'</dd><dl>')
-                .append('<dl><dt class="onecol">route</dt><dd>'+this.trains[i].route+'</dd><dl>')
-                .append('<dl><dt class="onecol">info</dt><dd>'+this.trains[i].info+'</dd><dl>');
+                .append('<dl><dt class="onecol">service</dt><dd>'+this.trains[i].service+'</dd></dl>')
+                .append('<dl><dt class="onecol">eta</dt><dd>'+this.trains[i].eta+'</dd></dl>')
+                .append('<dl><dt class="onecol">scheduled</dt><dd>'+this.trains[i].scheduled+'</dd></dl>')
+                .append('<dl><dt class="onecol">due</dt><dd>'+this.trains[i].due+'</dd></dl>')
+                .append('<dl><dt class="onecol">route</dt><dd>'+this.trains[i].route+'</dd></dl>')
+                .append('<dl><dt class="onecol">info</dt><dd>'+this.trains[i].info+'</dd></dl>');
               ol.append(li);
           };
           direction_div.append(ol);
